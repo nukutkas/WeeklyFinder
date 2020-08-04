04c71e8ba5824a9da2a6bb69d4780d121e4ca547
Nikita Kochetov <nothsmple@mail.ru>
2020-08-04 09:16:48 +0300

    Initial Commit

---
+++ WeeklyFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by Татьяна Кочетова on 04.08.2020.
+//  Copyright © 2020 Nikita Kochetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
